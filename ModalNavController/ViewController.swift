87a214123bdaed1ea0aa9d77a1aec63847cb7bec
Alex Tang <altitude@funkware.com>
2018-01-17 12:33:16 -0800

    Initial Commit

---
+++ ModalNavController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ModalNavController
+//
+//  Created by Alex Tang on 1/17/18.
+//  Copyright © 2018 Funkware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
